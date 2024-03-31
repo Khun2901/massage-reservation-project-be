d28b010523f178bf024baf21f49886c1e08d7b80
khunnnnnnn <nunnun.234@gmail.com>
2024-03-31 23:32:32 +0700

    Connect BE server to mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
-dotenv.config({path: './config/config.'})
+// Load env vars
+dotenv.config({path: './config/config.env'})
+
+//Connect to Database
+connectDB()
 
 const app = express()
 
+// Mock request
 app.get('/', (req, res) => {
     res.status(200).send({
         success: true,
@@ -16,4 +22,10 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT))
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT))
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    server.close(() => process.exit(1))
+})
